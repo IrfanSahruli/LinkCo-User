4e0b2a7a2885b08a1f7d7c9c35b544819891914a
IrfanSahruli <irfansahruli244@gmail.com>
2025-07-07 15:43:39 +0700

    revisi login

--- src/pages/Auth/Login.tsx
+++ src/pages/Auth/Login.tsx
@@ -5,17 +5,17 @@ const Login = () => {
     return (
         <div className='bg-blue-950 min-h-screen'>
             <div className='grid grid-cols-1 md:grid-cols-2'>
-                <div className='flex justify-center pt-10 px-4 md:py-[220px]'>
+                <div className='flex justify-center pt-10 px-4 md:py-[210px]'>
                     <img
                         src='LogoLinkCo.png'
                         className='text-white w-[100px] h-[100px] md:w-[220px] md:h-[220px] text-center font-extrabold'
                     />
                 </div>
                 <div className='flex justify-center mt-[50px] md:mt-[40px]'>
-                    <div className='bg-white rounded-t-4xl h-[637px] w-full px-8 py-10 md:py-8 md:rounded-4xl 
+                    <div className='bg-white rounded-t-4xl h-[637px] w-full px-8 py-6 md:py-8 md:rounded-4xl 
                         md:h-[570px] md:w-[500px]'
                     >
-                        <h1 className='text-[40px] text-center font-bold'>
+                        <h1 className='text-[30px] text-center font-bold'>
                             Login
                         </h1>
                         <p className='text-[18px] text-center text-gray-600 mt-6'>
@@ -24,32 +24,32 @@ const Login = () => {
                         <div className='flex flex-col mt-10'>
                             <label
                                 htmlFor="email"
-                                className='text-[20px] font-semibold'
+                                className='text-[18px] font-semibold'
                             >
                                 Email
                             </label>
                             <input
                                 type="text"
                                 placeholder='Email...'
-                                className='rounded-4xl border h-[50px] px-6 text-[23px]'
+                                className='rounded-4xl border h-[40px] px-6 text-[23px]'
                             />
                         </div>
                         <div className='flex flex-col mt-4'>
                             <label
                                 htmlFor="password"
-                                className='text-[20px] font-semibold'
+                                className='text-[18px] font-semibold'
                             >
                                 Password
                             </label>
                             <input
                                 type="text"
                                 placeholder='Password...'
-                                className='rounded-4xl border h-[50px] px-6 text-[23px]'
+                                className='rounded-4xl border h-[40px] px-6 text-[23px]'
                             />
                         </div>
                         <div className='mt-10 flex justify-center'>
                             <button className='bg-blue-800 rounded-4xl w-[360px] py-3'>
-                                <p className='font-semibold text-[23px] text-white'>
+                                <p className='font-semibold text-[18px] text-white'>
                                     Login
                                 </p>
                             </button>
